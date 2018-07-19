b1f4118092b83e787f6afafd7f3f335e843b5aa8
Derick Ostrenko <fredeerock@dmbp.local>
2018-07-19 09:34:28 -0500

    footer getting there

--- wp_data/wp-content/themes/dacwp/footer.php
+++ wp_data/wp-content/themes/dacwp/footer.php
@@ -1,10 +1,10 @@
 </div> <!-- #wrapper -->
 
-
-
 <footer>
-    <img height="77" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/dac_logo.png">
-    <img height="77" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/sig_logo.png">
+    <div id="footer-contents">
+        <img height="77" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/dac_logo.png">
+        <img height="77" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/sig_logo.png">
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
